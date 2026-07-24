7e65f72f8644eb97f279313350ef31e0bbed5dff
replityash5 <replityash5@gmail.com>
2026-07-24 11:20:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ function escapeHtml(s) {
   return String(s ?? '')
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
-    .replace/>/g, '&gt;');
+    .replace(/>/g, '&gt;');
 }
 
 function formatCompact(n) {
